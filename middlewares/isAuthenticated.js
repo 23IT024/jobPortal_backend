42cc66505a68f9bf207abb6f1f940714194ca510
23IT024 <kanish.s2023it@sece.ac.in>
2025-01-31 14:28:47 +0530

    new change

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -2,24 +2,26 @@ import jwt from "jsonwebtoken";
 
 const isAuthenticated = async (req, res, next) => {
     try {
-        const token = req.cookies.token;
-        if (!token) {
-            return res.status(401).json({
-                message: "User not authenticated",
-                success: false,
-            })
-        }
-        const decode = await jwt.verify(token, process.env.SECRET_KEY);
-        if(!decode){
-            return res.status(401).json({
-                message:"Invalid token",
-                success:false
-            })
-        };
-        req.id = decode.userId;
-        next();
+      const token = req.cookies.token;
+      if (!token) {
+        return res.status(401).json({
+          message: "User not authenticated",
+          success: false,
+        });
+      }
+      const decode = await jwt.verify(token, process.env.SECRET_KEY);
+      if (!decode) {
+        return res.status(401).json({
+          message: "Invalid token",
+          success: false,
+        });
+      }
+      req.id = decode.userId;
+      next();
     } catch (error) {
-        console.log(error);
+      console.error("Token verification error:", error);
+      return res.status(500).json({ message: "Internal server error" });
     }
-}
+  };
+  
 export default isAuthenticated;
